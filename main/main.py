ec628bcd80868981f366e58a76d87ed60e768434
Tran Khanh <tran_khanh@outlook.com>
2021-05-10 21:13:03 +0700

    setup python vevn 3.8.5 python

--- main/main.py
+++ main/main.py
@@ -1,7 +1,28 @@
 # save this as app.py
 from flask import Flask, escape, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from sqlalchemy.sql.schema import UniqueConstraint
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URL"] = "mysql://root:root@db/main"
+CORS(app)
+
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Interger)
+
+    UniqueConstraint('user_id', "product_id", name='user_product_unique')
 
 
 @app.route('/')
