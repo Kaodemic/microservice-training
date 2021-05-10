ad3b1cac9bce863e8b07e68d89d7c9b67e2a8232
Tran Khanh <tran_khanh@outlook.com>
2021-05-10 15:56:59 +0700

    update POST test

---
+++ CURL.md
@@ -0,0 +1,1 @@
+ curl -d '{"title":"title", "image":"image"}' -H "Content-Type: application/json" -X POST http://localhost:8000/api/products
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-
 from django import views
+
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
+
 from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
@@ -15,7 +17,10 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request):  # /api/products
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrive(self, request, pk=None):  # /api/products/<string:id>
         pass
@@ -25,3 +30,4 @@ class ProductViewSet(viewsets.ViewSet):
 
     def destroy(self, request, pk=None):  # /api/products/<string:id>
         pass
+
